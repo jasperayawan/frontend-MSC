6ff9d2c17d917345c7065cc820061a1883b9fe91
jasperayawan <ejayawan22@gmail.com>
2023-11-14 00:14:26 +0800

    UPDATE DOTENV

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const parserServer = require('parse-server').ParseServer;
 const parseDashboard = require('parse-dashboard')
 const userAuth = require('./routes/user')
-
-const port = 8383;
+const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
 
 /**middleware */
-
+app.use(cors({ origin: "http://localhost:5173" }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -20,7 +22,7 @@ app.listen(port, () => {
 
 
 const api = new parserServer({
-    databaseURI: "mongodb+srv://ejayawan22:qhlQQ72Obehoig6U@cluster0.f0mr86i.mongodb.net/?retryWrites=true&w=majority",
+    databaseURI: process.env.MONGODB_URI,
     appId: "123",
     masterKey: "1234",
     serverURL: `http://localhost:${port}/parse`,
